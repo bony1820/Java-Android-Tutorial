60b5d308f5d9b482300d5765d2ae102a6d5d24a3
tuyenbug <tuyen.nguyen5@ntq-solution.com.vn>
2022-08-10 23:14:06 +0700

    Done

--- app/src/main/java/com/example/java/android/tutorial/MainActivity.java
+++ app/src/main/java/com/example/java/android/tutorial/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.java.android.tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
+    BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context c, Intent intent) {
+            boolean success = intent.getBooleanExtra(
+                    WifiManager.EXTRA_RESULTS_UPDATED, false);
+            if (success) {
+                scanSuccess();
+            } else {
+                // scan failure handling
+                scanFailure();
+            }
+        }
+    };
+
+    WifiManager wifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wifiManager = (WifiManager)
+                getBaseContext().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        ActivityCompat.requestPermissions(this,
+                new String[] {
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_WIFI_STATE,
+                        Manifest.permission.ACCESS_NETWORK_STATE
+                }, 1);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        getBaseContext().registerReceiver(wifiScanReceiver, intentFilter);
+
+        boolean success = wifiManager.startScan();
+        if (!success) {
+            // scan failure handling
+            scanFailure();
+        }
+    }
+
+    private void scanSuccess() {
+        List<ScanResult> results = wifiManager.getScanResults();
+        for (int i = 0; i < results.size(); i++) {
+            Log.d(TAG, "scanSuccess: " + results.get(i).SSID + "-" + results.get(i).BSSID);
+        }
+    }
+
+    private void scanFailure() {
+        // handle failure: new scan did NOT succeed
+        // consider using old scan results: these are the OLD results!
+        List<ScanResult> results = wifiManager.getScanResults();
+        for (int i = 0; i < results.size(); i++) {
+            Log.d(TAG, "scanFailure: " + results.get(i).SSID);
+        }
     }
 }
